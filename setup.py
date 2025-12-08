16859fe25356ee7f079a4ef50811b41fbbe7fa92
vivechna202 <vivechanas45@gmail.com>
2025-12-08 11:32:33 +0530

    added dataset , loggers , exception and utils

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="us_visa",
+    version="0.0.1",
+    author="Vivechana Singh",
+    author_email="vivechanas45@gmail.com",
+    packages=find_packages()
+)
